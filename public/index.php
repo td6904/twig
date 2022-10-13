debda4d285595960966356e6dace0f443ad47665
td6904 <td665@pm.me>
2022-10-13 12:06:31 +0200

    Product file working, managed to show list and dump

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
-
-$name = 'Wilder';
+$name = 'Tyler';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
